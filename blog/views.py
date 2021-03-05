5c6984999f652d7e7c0e76ec186896a6f7504cba
adel-ks <adelmanaeva@gmail.com>
2021-03-05 19:23:08 +0600

    Внесли изменения в файлы views и post_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def home(request):
 	context = {
-	'variable': 'abc'
+	'variable': 'abc',
+	'username': 'Adel',
+	'my_friends':['Sasha','Gosha','Dima']
 	}
 	# return HttpResponse('<h1>Hello</h1>')
 	return render(request, 'blog/index.html', context)
 
 
 def post_list(request):
-	return render(request, "blog/posts_list.html")
+	get_posts = Post.objects.all()
+	return render(request, "blog/posts_list.html",{'get_posts': get_posts})
