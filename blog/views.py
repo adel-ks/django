7c11db65bcfde6400f0204b51e2d16becd045f85
adel-ks <adelmanaeva@gmail.com>
2021-03-05 18:47:42 +0600

    Создан новый файл index.html

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 
 
 def home(request):
+	context = {
+	'variable': 'abc'
+	}
 	# return HttpResponse('<h1>Hello</h1>')
-	return render(request, 'blog/index.html')
+	return render(request, 'blog/index.html', context)
 
 
 def post_list(request):
