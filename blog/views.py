1b45416639ba05607552a2b3c76ca16b84517380
adel-ks <adelmanaeva@gmail.com>
2021-03-10 19:22:12 +0600

    В файле views создали post_details

--- blog/views.py
+++ blog/views.py
@@ -16,3 +16,24 @@ def home(request):
 def post_list(request):
 	get_posts = Post.objects.all()
 	return render(request, "blog/posts_list.html",{'get_posts': get_posts})
+
+
+def search_form(request):
+	return render('search_form.html')
+
+
+# def search(request):
+# 	if 'q' in request.GET['q']:
+# 		q = request.GET['q']
+# 		posts = Post.objects.filter(title_icontains=q)
+# 		return render('search_results.html', {'posts': posts, 'query':q})
+# 	else:
+# 		return render	 
+
+
+def post_details(request, post_id):
+	post = Post.objects.get(id = post_id)
+	context = {
+	'post': post
+	}
+	return render(request, 'blog/post_details.html', context)
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import home, post_list
+from blog.views import home, post_list,post_details
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home),
     path('blog/', post_list),
+    path('blog/<post_id>/', post_details),
 ]
